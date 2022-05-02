366b27b627939aa65c26a5f80240330313e7a7ff
el-presidente-kukki <67149799+el-presidente-kukki@users.noreply.github.com>
2022-05-02 22:02:34 +0200

    Added mailhandler
    - Using Yagmail and own Domain

---
+++ Flask/BackendHelper/mail/mailhandler.py
@@ -0,0 +1,46 @@
+import yagmail
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# Yagmail -> Magic Wrapper around smtplib's SMTP connection and allows messages to be sent
+helpdesk = yagmail.SMTP(os.getenv('mail'), os.getenv('mail_pass'), host='smtp.strato.de', port=465, timeout=120)
+
+
+def send_mail(to, subject, body):
+    # Send Mail to the given recipient
+    helpdesk.send(to, subject, body)
+
+# Welcome Mail to a new user containing his/her username
+def welcome_mail(to, Name):
+    # Send Welcome Mail to the given recipient
+    subject = "Herzlich Willkommen bei RescueMe! :)"
+    body = """\
+    <html>
+      <head></head>
+      <body>
+        <p>Herzlich Willkommen {}<br>
+           Es freut uns Sie bei RescueMe begrüßen zu dürfen!<br>
+           Hier ist ihr  <a href="https://puginarug.com/">Registrierungslink</a>!
+        </p>
+      </body>
+    </html>
+    """.format(Name)
+    send_mail(to, subject, body)
+
+
+# Password Reset Mail to a specific user containing a new password
+def pw_reset_mail(to, pw):
+    subject = "RescueMe Passwort zurücksetzen! :O"
+    body = """\
+    <html>
+      <head></head>
+      <body>
+        <p>Hallo,<br>
+           Vielen Dank für ihre Anfrage.<br>
+           Hier ist Ihr neues Passwort: {}<br>
+        </p>
+      </body>
+    </html>
+    """.format(pw)
+    send_mail(to, subject, body)
