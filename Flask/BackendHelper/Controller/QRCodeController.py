1e4a38a3eb3d4eea1b10e6e983fb5327de45a6be
Robin <robin.herrmann1@gmx.de>
2022-05-14 17:26:50 +0200

    fixed minor bugs at RSA global fernet-decryption
    Signed-off-by: Robin <robin.herrmann1@gmx.de>

--- Flask/BackendHelper/Controller/QRCodeController.py
+++ Flask/BackendHelper/Controller/QRCodeController.py
@@ -18,11 +18,11 @@ class QRCodeController:
 
         try:
             if db.session.query(FernetKeys.FernetKeys.email).filter_by(email=user_mail).first() is not None:
-                fernerQuery = db.session.query(FernetKeys.FernetKeys).filter(FernetKeys.FernetKeys.email == user_mail).all()
-                fernerQuery = fernerQuery[0]
+                fernetQuery = db.session.query(FernetKeys.FernetKeys).filter(FernetKeys.FernetKeys.email == user_mail).all()
+                fernetQuery = fernetQuery[0]
 
-                fernet = fernerQuery.fernet
-                decryptedFernet = decryptKeyForDb(privateKey=os.getenv("PRIVATEKEY"), encryptedFernet=fernet)
+                fernet = fernetQuery.fernet
+                decryptedFernet = decryptKeyForDb(privateKey=os.getenv("PRIVATEKEY").encode("utf-8"), encryptedFernet=fernet)
 
                 result = db.session.query(User.User).filter(User.User.email == user_mail).all()
                 user_info = result[0]
@@ -50,28 +50,18 @@ class QRCodeController:
     @staticmethod
     def readQRCode():
         user_email = request.args.get('email')
-        INPUT = request.args.get('input')
+        user_data = request.args.get('input')
 
         result = db.session.query(FernetKeys.FernetKeys).filter(FernetKeys.FernetKeys.email == user_email).all()
         user_info = result[0]
-
-
 
         fernet = user_info.fernet
         fernet_rsa_decrypted = decryptKeyForDb(os.getenv('PRIVATEKEY').encode("utf-8"), fernet)
         fernet_decrypted = pickle.loads(fernet_rsa_decrypted)
         print(fernet_decrypted)
 
-
-
-        decryptedJSON = decryptData(INPUT, fernet_decrypted)
+        decryptedJSON = decryptData(user_data, fernet_decrypted)
         data = json.loads(decryptedJSON)
         print(data)
 
         return 'Decryption success'
-
-        # encryptedJSON = b'gAAAAABias2I9qSxvMH8eg7WT6f3-xrDLgMxIY5yhIQkUU0sy1v_R7-HDa_LdcBGd4v1Tkx27hBnDuwFuGYwjAvtTDSnQDujKiQ_n4GzDexjz08qZGKkBbqz9DFROmZzbBlFtOf1hNvK'
-
-        # with open('BackendHelper/Keys/filekey.key', 'rb') as filekey:
-        #    key = filekey.read()
-
