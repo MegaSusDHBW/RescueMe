c086fe3b441c8e29c315f433db85005735795ae0
el-presidente-kukki <67149799+el-presidente-kukki@users.noreply.github.com>
2022-05-17 20:35:24 +0200

    Added get Neares Hospitals from Location (lat/lng)

---
+++ Flask/BackendHelper/Location/hospital.py
@@ -0,0 +1,31 @@
+from googleplaces import GooglePlaces, types, lang
+import requests
+import json
+
+# Google Places API Todo: Move to .env file
+API_KEY = 'Empty'
+
+# Initialising the GooglePlaces constructor
+google_places = GooglePlaces(API_KEY)
+
+
+def get_hospital_query_result(lat, lng):
+    # Query the Google Places API
+    try:
+        query_result_hospital = google_places.nearby_search(
+            lat_lng={'lat': lat, 'lng': lng},
+            radius=5000,
+            types=[types.TYPE_HOSPITAL])
+    except Exception as e:
+        print('Could not get hospital query result: ', e)
+        return None
+
+    query_dict = {}
+    i = 0
+    for place in query_result_hospital.places:
+        query_dict[i] = [place.name, str(place.geo_location['lat']), str(place.geo_location['lng'])]
+        i += 1
+
+    json_hospital = json.dumps(query_dict, indent=4, ensure_ascii=False)
+
+    return json_hospital
