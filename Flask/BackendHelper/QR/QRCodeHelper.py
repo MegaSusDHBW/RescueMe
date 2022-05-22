8cfbef426dbac53820db5e928cb4b3825a738860
Robin <robin.herrmann1@gmx.de>
2022-05-22 21:31:48 +0200

    added attributes to QR-Code Dict
    Signed-off-by: Robin <robin.herrmann1@gmx.de>

--- Flask/BackendHelper/QR/QRCodeHelper.py
+++ Flask/BackendHelper/QR/QRCodeHelper.py
@@ -1,7 +1,6 @@
 import json
 
 import qrcode
-from PIL import Image
 
 from Flask.BackendHelper.Cryptography.CryptoHelper import encryptData
 
@@ -15,12 +14,28 @@ def generateDictForQRCode(user_info):
         qrcode_dict.update({"lastname": user_info.healthData.lastname})
         qrcode_dict.update({"organDonorState": user_info.healthData.organDonorState})
         qrcode_dict.update({"bloodGroup": user_info.healthData.bloodGroup})
+        qrcode_dict.update({"birthDate": user_info.healthData.birthdate})
     if user_info.emergencyContact:
         qrcode_dict.update({"emergencyEmail": user_info.emergencyContact.email})
         qrcode_dict.update({"emergencyFirstname": user_info.emergencyContact.firstname})
         qrcode_dict.update({"emergencyLastname": user_info.emergencyContact.lastname})
         qrcode_dict.update({"emergencyBirthday": user_info.emergencyContact.birthdate})
         qrcode_dict.update({"emergencyPhone": user_info.emergencyContact.phonenumber})
+    if user_info.healthData.allergies:
+        allergies = []
+        for a in user_info.healthData.allergies:
+            allergies.append({"title": a.name})
+        qrcode_dict.update({"allergies": allergies})
+    if user_info.healthData.diseases:
+        diseases = []
+        for d in user_info.healthData.diseases:
+            diseases.append({"title": d.name})
+        qrcode_dict.update({"diseases": diseases})
+    if user_info.healthData.vaccines:
+        vaccines = []
+        for v in user_info.healthData.vaccines:
+            vaccines.append({"title": v.name})
+        qrcode_dict.update({"diseases": vaccines})
 
     return qrcode_dict
 
@@ -35,19 +50,9 @@ def createQRCode(qrcode_dict, fernet):
     # bytes in string umwandeln
     encryptedJSON = encryptedJSON.decode('utf-8')
     print(encryptedJSON)
-    qr = qrcode.QRCode(
-        error_correction=qrcode.constants.ERROR_CORRECT_H
-    )
     qr = qrcode.QRCode(box_size=100, border=1)
     qr.add_data(encryptedJSON)
-    logo = Image.open('../static/img/logo-qr.png')
-
     img = qr.make_image()
-    pos = ((img.size[0] - logo.size[0]) // 2,
-           (img.size[1] - logo.size[1]) // 2)
-    img = qr.make_image()
-    img.paste(logo, pos)
-
 
     try:
         img.save('../static/img/qrcode.png')
